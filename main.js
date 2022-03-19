24738187a8360aa23edb46ed52d16701a7f31ca6
Nathan McCune <natham9@uw.edu>
2022-03-19 16:30:46 -0700

    int

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
-ASSET_MANAGER.queueDownload("./raptor_easy.png");
+ASSET_MANAGER.queueDownload("./walking_sprite.png");
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
-
+	ctx.imageSmoothingEnabled = false;
 	gameEngine.addEntity(new raptor_easy(gameEngine));
 
 	gameEngine.init(ctx);
--- raptor_easy.js
+++ raptor_easy.js
@@ -1,12 +1,16 @@
 class raptor_easy {
     constructor(game){
         this.game = game;
-        this.animator = new Animator(ASSET_MANAGER.getAsset("./raptor_easy.png"), 0, 0, 55, 56, 5, 0.2);
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./walking_sprite.png"), 0, 0, 33, 50, 11, 0.4);
+        this.x = 0;
+        this.y = 0;
+        this.speed = 50;
     };
     update(){
-
+        this.x += this.speed * this.game.clockTick;
+        if (this.x > 1024) this.x = 0;
     };
     draw (ctx){
-        this.animator.drawFrame(this.game.clockTick, ctx, 25,25);
+        this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y);
     };
 }
